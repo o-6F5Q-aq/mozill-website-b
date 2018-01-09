0d4b4d2b594da30e7fdb47ada4d070dde8fc6a65
o-6F5Q-aq <stephane.genot@gmail.com>
2018-01-09 16:07:27 -0500

    new popup alerts on images

--- scripts/names.js
+++ scripts/names.js
@@ -2,9 +2,15 @@
 function sayOuch() {
     alert('Ouch! Stop poking us!');
 }
+function sayBoss(){
+	alert("Yup. I'm the boss.")
+}
 
 // Use a CSS selector to identify an element
-var foxImage = document.querySelector('img');
+var portraitimage = document.querySelector('img');
+var cartoonimage = document.querySelector('img')
 
 // Assign the function to the onclick event on that element
-foxImage.onclick = sayOuch;
+portraitimage.onclick = sayOuch;
+
+cartoonimage.onclick = sayBoss;
